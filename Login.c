2c022129cc05e6888be3866c76e03abd5baff5b4
Tengku Arya Mukasyaf <aryamukasyaf@yahoo.com>
2023-12-04 04:18:43 +0300

    add everything

---
+++ Login.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<string.h>
+#include"Register.h"
+#include"Login.h"
+int size;
+char Name[50];
+char Passp[50];
+int main() {
+    start();
+
+    printf("\033[2J\033[1;1H");
+
+    /*if (strcmp(Name, "admin") == 0 && strcmp(Passp, "admin") == 0) {
+        printf("WElCOM DEAR ADMIN");
+    }
+    else {
+        printf("WElCOM DEAR Client");
+    }
+    */
+}
